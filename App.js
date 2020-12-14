6862f90463e047ec5c95e6e732168e8c25b087fa
junjie <jun_jie1994@hotmail.com>
2020-12-14 12:41:45 +0800

    bustop app

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=46679";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
+
+  function loadBusStopData() {
+    setLoading(true);
+
+    fetch(BUSSTOP_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        //console.log(responseData)
+        const myBus = responseData.services.filter(
+          (item) => item.no === "912"
+        )[0];
+        console.log(myBus);
+        setArrival(myBus.next.time);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    loadBusStopData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator size="large" color="#aa8" /> : arrival}
+      </Text>
+      <TouchableOpacity onPress={null} style={styles.button}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +49,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  arrivalTime: {
+    fontSize: 36,
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 24,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
